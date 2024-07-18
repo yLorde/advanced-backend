a666953935e3059177dd3c55d624bf74ebb2a340
lord <davisouza2204@gmail.com>
2024-07-17 23:40:35 -0300

    removi colors

--- src/database/indexDb.js
+++ src/database/indexDb.js
@@ -3,8 +3,8 @@ const auth = require('../data/auth');
 
 module.exports.dbConnect = async () => {
     await mongo.connect(auth.mongo).then(async (connect) => {
-        console.log(color.green(`[API] (DATABASE) -> Conectado`));
-        console.log(color.green(`[API] (DATABASE) -> Conectado ao ${connect.connection.db.databaseName}`));
+        console.log(`[API] (DATABASE) -> Conectado`);
+        console.log(`[API] (DATABASE) -> Conectado ao ${connect.connection.db.databaseName}`);
     }).catch(async (err) => {
         if (err) {
             console.log(`Error (DataBase): ${err}`);
